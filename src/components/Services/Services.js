1de8853c69fc35f32973298483320ec3308c3e3d
kirancodes <kiran404.kbk@gmail.com>
2022-09-27 18:47:15 +0530

    changes

--- src/components/Services/Services.js
+++ src/components/Services/Services.js
@@ -1,36 +1,39 @@
-import React, { useState } from "react"
+import React, { useState } from "react";
 import Base from "../Base/Base";
 import ServiceList from "../Home/serviceList";
 import "./style/services.css";
 
 const Services = () => {
-    const [counting, setCounting] = useState(150)
+  const [counting, setCounting] = useState(150);
 
-    var timesRun = counting;
-    var interval = setInterval(function () {
-        if (timesRun === 154) {
-            clearInterval(interval);
-        }
-        else {
-            setCounting(timesRun += 1)
-        }
-    }, 1600);
+  var timesRun = counting;
+  var interval = setInterval(function () {
+    if (timesRun === 154) {
+      clearInterval(interval);
+    } else {
+      setCounting((timesRun += 1));
+    }
+  }, 1600);
 
-    return (
-        <Base>
-            <div className="serviceList">
-
-
-                <ServiceList
-                    heading={<> Our <span>Concern</span> Is Your Satisfaction  </>}
-                    content="Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classNameical Latin literature from 45 BC, making it over 2000 years old. Richard McClintock, a Latin professor at Hampden-Sydney College in Virginia"
-                />
-                <h2 className="text-center text-capitalize">
-                    We have <span>{counting}+</span> happy Customers, <span> Still Counts. </span>
-                </h2>
-            </div>
-        </Base>
-    )
-}
+  return (
+    <Base>
+      <div className="serviceList">
+        <ServiceList
+          heading={
+            <>
+              {" "}
+              Our <span>Concern</span> Is Your Satisfaction{" "}
+            </>
+          }
+          content="Knowing  what our customers want most and what our company does best, we focus on where those two meet"
+        />
+        <h2 className="text-center text-capitalize">
+          We have <span>{counting}+</span> happy Customers,{" "}
+          <span> Still Counts. </span>
+        </h2>
+      </div>
+    </Base>
+  );
+};
 
 export default Services;
